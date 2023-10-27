032a9474596dce8530ce9f3787f89bf4c7ac32cb
Zhicheng <shi199387@gmail.com>
2023-10-27 00:38:07 -0400

    modified Color Flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025']
+
+let color = document.querySelector('.color')
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click', function () {
+  const randomNumber = getRandom()
+  document.body.style.backgroundColor = colors[randomNumber]
+  color.textContent = colors[randomNumber]
+})
+
+function getRandom() {
+  return Math.floor(Math.random() * colors.length)
+}
