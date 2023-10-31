3bcaf5a5ace92dab977810fe1605ec68da875acf
Zhicheng <shi199387@gmail.com>
2023-10-31 01:59:59 -0400

    finished Navbar project

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,15 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const links = document.querySelector('.links')
+const btn = document.querySelector('.nav-toggle')
+
+btn.addEventListener('click', function () {
+  links.classList.toggle('show-links')
+  // if (links.classList.contains('show-links')) {
+  //   links.classList.remove('show-links')
+  // } else {
+  //   links.classList.add('show-links')
+  // }
+})
